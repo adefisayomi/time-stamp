ff8981adf77ed9e267fde3cd442dc1d47aa18379
Adefisayomi Oluwole <adefisayomioluwole@gmail.com>
2020-12-30 23:13:43 +0100

    added a default get route

--- server.js
+++ server.js
@@ -50,33 +50,33 @@ else {
 }
 })
 
-app.post('/api/timestamp/:date', (req, res) => {
+// app.post('/api/timestamp/:date', (req, res) => {
 
-    if (req.body.timeStamp && (req.body.timeStamp).includes('-')) {
-            const date = new Date(req.body.timeStamp)
+//     if (req.body.timeStamp && (req.body.timeStamp).includes('-')) {
+//             const date = new Date(req.body.timeStamp)
             
-            if (date == 'Invalid Date' || date == NaN) {
-                res.json({ error: 'Invalid Date' })
-            }
-            else {
-                res.json({ unix: date.valueOf(), utc: date.toUTCString() })
-            }
-        }
+//             if (date == 'Invalid Date' || date == NaN) {
+//                 res.json({ error: 'Invalid Date' })
+//             }
+//             else {
+//                 res.json({ unix: date.valueOf(), utc: date.toUTCString() })
+//             }
+//         }
 
-    if (req.body.timeStamp && (req.body.timeStamp).includes('-') == false ) {
-            const date = new Date(parseInt(req.body.timeStamp))
-            if (date == 'Invalid Date' || date == NaN) {
-                res.json({ error: 'Invalid Date' })
-            }
-            else {
-                res.json({ unix: date.valueOf(), utc: date.toUTCString() })
-            }
-        }
+//     if (req.body.timeStamp && (req.body.timeStamp).includes('-') == false ) {
+//             const date = new Date(parseInt(req.body.timeStamp))
+//             if (date == 'Invalid Date' || date == NaN) {
+//                 res.json({ error: 'Invalid Date' })
+//             }
+//             else {
+//                 res.json({ unix: date.valueOf(), utc: date.toUTCString() })
+//             }
+//         }
 
-    else {
-         const defaultDate = new Date()
-         res.json({ unix: defaultDate.valueOf(), utc: defaultDate.toUTCString() }) 
-        }
-})
+//     else {
+//          const defaultDate = new Date()
+//          res.json({ unix: defaultDate.valueOf(), utc: defaultDate.toUTCString() }) 
+//         }
+// })
 
 app.listen(PORT, console.log('Server started on PORT: '+ PORT + ' ...'))
